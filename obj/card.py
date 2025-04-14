f3161253c902967c8ea58e43949ae95a01b85b11
Diomedes Git <diomedesgit@gmail.com>
2025-04-14 22:28:06 +0100

    methods etc

--- obj/card.py
+++ obj/card.py
@@ -7,11 +7,34 @@ class Card:
             value - int, numeric value of card
             ordering - position of card in 'properly' sorted deck, 0-len(deck)
             (possibly trying a mapped 0-10 range, but probably not at first)
-        
+            
+            a few methods that might be useful
         """
+        
+        
         self.name = name
         self.suit = suit
         self.rank = rank
         self.value = value
         self.ordering = ordering
+        
         pass
+    
+    def __repr__(self):
+         return f'<card: {self.name}>'
+     
+    def __eq__(self, other):
+        return (
+            isinstance(other, Card) and 
+            self.name == other.name and 
+            self.suit and other.suit and 
+            self.rank and other.rank
+            )
+    
+    
+    
+    def is_same_suit(self, other):
+        if isinstance(other, Card):
+            return self.suit == other.suit
+        return False
+    
