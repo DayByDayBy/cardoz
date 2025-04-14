df413c3804da23d1acc083919bf5a9e620d83f5c
Diomedes Git <diomedesgit@gmail.com>
2025-04-14 22:37:55 +0100

    getters etc

--- obj/card.py
+++ obj/card.py
@@ -5,20 +5,34 @@ class Card:
         
             name/suit/rank - full name of card, suit, rank-name (eg K)
             value - int, numeric value of card
-            ordering - position of card in 'properly' sorted deck, 0-len(deck)
-            (possibly trying a mapped 0-10 range, but probably not at first)
+            order_pos - position of card in 'properly' sorted deck, likely 0-len(deck)
+            (possibly trying a mapped 0-10 range later, but probably not at first)
             
             a few methods that might be useful
         """
-        
         
         self.name = name
         self.suit = suit
         self.rank = rank
         self.value = value
-        self.ordering = ordering
-        
+        self.order_pos = ordering
         pass
+    
+    
+    # getters:
+    
+    def get_name(self):
+        return self.name
+    def get_suit(self):
+        return self.suit
+    def get_rank(self):
+        return self.rank
+    def get_value(self):
+        return self.value
+    def get_ordering(self):
+        return self.order_pos
+    
+    # object checks:
     
     def __repr__(self):
          return f'<card: {self.name}>'
@@ -32,9 +46,18 @@ class Card:
             )
     
     
+    # card checks:
     
     def is_same_suit(self, other):
         if isinstance(other, Card):
             return self.suit == other.suit
         return False
+    
+    def is_same_value(self, other):
+        if isinstance(other, Card):
+            return self.value == other.value
+        return False
+    
+    def is_diff_both(self, other):
+        return self.suit == other.suit and self.value == other.value
     
