baca564b666befd0e40338461388a9fb3d9b9282
Diomedes Git <diomedesgit@gmail.com>
2025-05-10 23:04:13 +0100

    etc

--- tools/add_idx_to_cards.py
+++ tools/add_idx_to_cards.py
@@ -8,3 +8,9 @@ print("tarot_deck = [")
 for card in updated_deck:
     print(f"    {card},")
 print("]")
+
+
+#  this will add the idx to the dict/json thing
+
+# tested it just to be sure, and it works as expected. will 
+# build a couple more tools to shape before i save it/replace it
