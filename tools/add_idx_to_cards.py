bfcd2b2bbc9b5104ea2cf8b40aff3632cd642c62
Diomedes Git <diomedesgit@gmail.com>
2025-05-10 23:09:50 +0100

    etc

--- tools/add_idx_to_cards.py
+++ tools/add_idx_to_cards.py
@@ -13,4 +13,5 @@ print("]")
 #  this will add the idx to the dict/json thing
 
 # tested it just to be sure, and it works as expected. will 
-# build a couple more tools to shape before i save it/replace it
+# build a couple more tools to shape it before i save 
+# it/replace it with a final/to be used version
