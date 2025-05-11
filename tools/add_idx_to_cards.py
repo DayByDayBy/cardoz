ea0bf601deb454501c05fb8290b928c3d5cb3338
Diomedes Git <diomedesgit@gmail.com>
2025-05-11 19:21:28 +0100

    etc

--- tools/add_idx_to_cards.py
+++ tools/add_idx_to_cards.py
@@ -12,6 +12,20 @@ print("]")
 
 #  this will add the idx to the dict/json thing
 
-# tested it just to be sure, and it works as expected. will 
+# tested it just to be sure, and it works as expected. might 
 # build a couple more tools to shape it before i save 
-# it/replace it with a final/to be used version
+# it/replace it with a final/to be used version. 
+# tools or not it will need edited
+
+# atm i think i want:  
+
+# type,
+# suit,
+# value,
+# nameShort,
+# name,
+# position,
+# meaningUp,
+# meaningRev,
+# description
+
